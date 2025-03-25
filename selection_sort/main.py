f14c46f4ebeb36ceeef6e1cb4ba8d6d0a53f380a
Jeremy <125468995+Jeremy-Tarlie@users.noreply.github.com>
2025-03-25 12:19:09 +0100

    Update main.py

--- selection_sort/main.py
+++ selection_sort/main.py
@@ -6,8 +6,10 @@ while y != "":
     y = input("Entrez un nombre: ")
 
 for i in range(len(x)):
-    for j in range(len(x)):
-        if x[i] < x[j]:
-            x[i], x[j] = x[j], x[i]
-            
+    min_index = i
+    for j in range(i + 1, len(x)):
+        if x[j] < x[min_index]:
+            min_index = j
+    x[i], x[min_index] = x[min_index], x[i]
+
 print(x)
